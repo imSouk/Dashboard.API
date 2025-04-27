3c41534d2a8077f27aa86a810aa8e2facb12fee5
Pedro <pedrohcjorge@gmail.com>
2025-04-27 00:15:45 -0300

    Create webapi +  context and connection string configuration

---
+++ Dashboard_webAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Dashboard_webAPI.Models
+{
+    public class User
+    {  
+        public int Id { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public User()
+        {
+            
+        }
+
+
+    }
+}
